07448494cb5964a74bbe6be81cd68ce09766cc98
pradeepkumar456 <adigangwar1@gmail.com>
2024-12-16 16:48:14 +0000

    commit

---
+++ 01_Basics/03_numbersAndmath.js
@@ -0,0 +1,9 @@
+const score = 100;
+console.log(score);
+
+const balance = new Number(100);
+console.log("This is my balance",balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(5));
+
