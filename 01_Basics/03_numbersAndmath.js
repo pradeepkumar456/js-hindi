811b4e7402d41e25e552df2114f2eab0444dc507
pradeepkumar456 <adigangwar1@gmail.com>
2024-12-16 17:13:15 +0000

    commit

--- 01_Basics/03_numbersAndmath.js
+++ 01_Basics/03_numbersAndmath.js
@@ -1,9 +1,34 @@
 const score = 100;
-console.log(score);
+// console.log(score);
 
-const balance = new Number(100);
-console.log("This is my balance",balance);
+// const balance = new Number(100);
+// console.log("This is my balance",balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(5));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(5));
+
+// const otherNumber = 89.2655;
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreads = 10000000;
+// console.log(hundreads.toLocaleString('en-IN')); // for indian precidence 
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.89));
+
+// console.log((Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() *(max - min + 1)+min ));
+
+
+
+
+
+
+
+
 
