28ce1a0840b478339e86683ba2113b2d93c05116
pradeepkumar456 <adigangwar1@gmail.com>
2024-12-16 15:35:53 +0000

    commit

---
+++ 01_Basics/02_conversionoperation.js
@@ -0,0 +1,10 @@
+let marks = true;
+console.log(typeof marks);
+console.log(typeof (marks));
+
+let valueInNumber = Number(marks);
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+ 
+
