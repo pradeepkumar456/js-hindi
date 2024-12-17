796d43407e38b4f891d198367e5adb0403e0399c
Pradeep Kumar <adigangwar1@gmail.com>
2024-12-17 06:20:26 +0000

    commit

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,44 @@
+const arr = [10,20,45,36,45];
+
+// const heros = ["spiderman ", "Palemaan "];
+
+// const arr2 = new Array(1,2,5,45);
+// console.log(arr2[2]);
+
+// methods 
+// arr.push(4);
+// arr.push(1000);
+// arr.push("Aditya kumar");
+// arr.pop();
+
+// arr.unshift(125);
+
+// arr.shift()
+
+// console.log(arr.includes(47));
+// console.log(arr.indexOf(10));
+
+// console.log(arr);
+
+// const newArr = arr.join();
+// console.log(arr);
+// console.log(typeof(newArr));
+
+// slice and splice 
+
+console.log("A" , arr);
+const numb1 = arr.slice(1,3);
+console.log(numb1);
+console.log("B",arr);
+
+const numb2 = arr.splice(1,3);
+console.log("C" , arr );
+console.log(numb2);
+
+
+
+
+
+
+
+
