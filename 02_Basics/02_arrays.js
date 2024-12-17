6ac78da7b335fb81303401c3a19e5e217beaa23d
Pradeep Kumar <adigangwar1@gmail.com>
2024-12-17 07:03:54 +0000

    commit

---
+++ 02_Basics/02_arrays.js
@@ -0,0 +1,28 @@
+const marbelHeros = [ "Thor ", "spiderman ", "Ironman "];
+const dcHeros  = ["superman ", "flash ", "batman "];
+
+// marbelHeros.push(dcHeros);
+// console.log(marbelHeros);
+
+// const myHeros = marbelHeros.concat(dcHeros);
+// console.log(myHeros);
+
+// const myNewHeros = [...marbelHeros , ...dcHeros];
+// console.log(myNewHeros);
+
+const anotheArrays = [20,10 , [10,14,36], [45,74,12],8, [1,4 ,[4,6]]];
+
+// const newArray = anotheArrays.flat(Infinity);
+// console.log(newArray);
+
+
+// console.log(Array.isArray("Pradeep"));
+// console.log(Array.from("Pradeep"));
+
+// console.log(Array.from( {name : "Pradeep"}));
+ 
+const score1 = 20;
+const score2 = 40;
+const score3 = 80;
+console.log(Array.of(score1, score2, score3));
+
